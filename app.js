2b656ffc78f929553205dedb890562e374edc070
Nic <user@USER-no-MacBook-Pro-2.local>
2020-09-28 22:14:05 +0900

    add editing function

--- app.js
+++ app.js
@@ -23,88 +23,140 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 //------------------------------------------------------------------
 
 
 
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
 
-    Post.find({}, function(err, retrievedContent){
-      //console.log(retrievedContent);
-      res.render("home", {content:retrievedContent});
-    })
+  Post.find({}, function(err, retrievedContent) {
+    //console.log(retrievedContent);
+    res.render("home", {
+      content: retrievedContent
+    });
+  })
 
 
 })
 
-app.get("/contact", function(req,res){
-  res.render("contact", {content: contactContent});
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    content: contactContent
+  });
 })
 
-app.get("/about", function(req, res){
-  res.render("about", {content:aboutContent});
+app.get("/about", function(req, res) {
+  res.render("about", {
+    content: aboutContent
+  });
 })
 
-app.get("/compose", function(req, res){
+app.get("/compose", function(req, res) {
   res.render("compose");
 })
 
-app.get("/post/:postID", function (req, res) {
+app.get("/post/:postID", function(req, res) {
 
   const requestedPostID = req.params.postID;
-  Post.findOne({_id: requestedPostID}, function(err, post){
-    if(err){
+  Post.findOne({
+    _id: requestedPostID
+  }, function(err, post) {
+    if (err) {
       console.log(err);
-    }else{
-    res.render("post", {
-    currentPostID: requestedPostID,
-    title: post.title,
-    content: post.content
-    });
-   }
+    } else {
+      res.render("post", {
+        currentPostID: requestedPostID,
+        title: post.title,
+        content: post.content
+      });
+    }
 
- });
+  });
 })
 
-app.post("/delete", function(req,res){
+app.post("/delete", function(req, res) {
   const currentPostID = req.body.deleteButton;
-  Post.deleteOne({_id: currentPostID}, function(err, post){
-    if(err){
+  Post.deleteOne({
+    _id: currentPostID
+  }, function(err, post) {
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       res.redirect("/");
     }
   })
-});
+})
 
+app.post("/edit", function(req, res) {
+  const currentPostID = req.body.editButton;
+  //console.log(currentPostID);
+  res.redirect("/edit/" + currentPostID);
+})
 
+app.get("/edit/:postID", function(req, res) {
+  const requestedPostID = req.params.postID;
+  Post.findOne({
+    _id: requestedPostID
+  }, function(err, post) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("edit", {
+        currentPostID: requestedPostID,
+        title: post.title,
+        content: post.content
+      });
+    }
 
-app.post("/compose", function(req,res){
+  });
+})
 
-  const composition =  new Post({title:req.body.compositionTitle , content:req.body.compositionPost});
-
-  composition.save(function(err){
-    if(!err){
-        console.log("database updated successfully");
+app.post("/update", function(req, res) {
+      const requestedPostID = req.body.updateButton;
+      revisedTitle = req.body.revisedTitle;
+      revisedPost= req.body.revisedPost;
+      var filter = { _id: requestedPostID };
+      var update = { $set: {title: revisedTitle, content: revisedPost }};
+      Post.updateOne(filter, update, function(err, post) {
+        if(err) {
+          console.log(err);
+      }else{
         res.redirect("/");
       }
-  });
-
-
-
-
+    })
 })
 
-app.post("/",function(req,res){
-  res.redirect("/compose");
-})
+      app.post("/compose", function(req, res) {
+
+        const composition = new Post({
+          title: req.body.compositionTitle,
+          content: req.body.compositionPost
+        });
+
+        composition.save(function(err) {
+          if (!err) {
+            console.log("database updated successfully");
+            res.redirect("/");
+          }
+        });
 
 
 
 
-//-------------------------------------------------------------------
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+      })
+
+      app.post("/", function(req, res) {
+        res.redirect("/compose");
+      })
+
+
+
+
+      //-------------------------------------------------------------------
+      app.listen(3000, function() {
+        console.log("Server started on port 3000");
+      });
