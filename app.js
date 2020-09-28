3f09a3de7cc967270323972a83398b37d6a313e1
Nic <user@USER-no-MacBook-Pro-2.local>
2020-09-28 18:06:41 +0900

    add db functionality, complete delete button functionality

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
+//require and initialize mongodb
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewURLParser: true
+});
 
+//establish content schema
+const postSchema = {
+  title: String,
+  content: String
+};
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const Post = mongoose.model("Post", postSchema);
 
 const app = express();
 
@@ -19,11 +27,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 //------------------------------------------------------------------
 
-let posts =[];
+
 
 app.get("/", function(req,res){
 
-    res.render("home", {content:posts});
+    Post.find({}, function(err, retrievedContent){
+      //console.log(retrievedContent);
+      res.render("home", {content:retrievedContent});
+    })
 
 
 })
@@ -42,30 +53,40 @@ app.get("/compose", function(req, res){
 
 app.get("/post/:postID", function (req, res) {
 
+  const requestedPostID = req.params.postID;
+  Post.findOne({_id: requestedPostID}, function(err, post){
+    if(err){
+      console.log(err);
+    }else{
+    res.render("post", {
+    currentPostID: requestedPostID,
+    title: post.title,
+    content: post.content
+    });
+   }
 
-  const postID = _.lowerCase(req.params.postID);
+ });
+})
 
-  for(var i = 0; i < posts.length; i++) {  //change to a forEach loop?
-
-
-    var postTitle = _.lowerCase(posts[i].title);
-
-    if (postTitle === postID) {
-        res.render("post", {title:posts[i].title, post:posts[i].post});
-        console.log("it's a match");
-        break;
-    }
-  }
-
+app.post("/post/:postID", function(req,res){
+  console.log(currentPostID);
 })
 
 
 
 app.post("/compose", function(req,res){
-  const composition = {title:req.body.compositionTitle , post:req.body.compositionPost};
 
-  posts.unshift(composition); //using unshift instead of push to make it update more like a regular blog
-  res.redirect("/");
+  const composition =  new Post({title:req.body.compositionTitle , content:req.body.compositionPost});
+
+  composition.save(function(err){
+    if(!err){
+        console.log("database updated successfully");
+        res.redirect("/");
+      }
+  });
+
+
+
 
 })
 
