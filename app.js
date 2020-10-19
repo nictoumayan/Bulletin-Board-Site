696fada06114bd697a97b963090e2ba8c47a559d
Nic <user@USER-no-MacBook-Pro-2.local>
2020-10-20 08:44:01 +0900

    switched to cloud-based database to host content

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const version = 2.2;
+const version = 2.5;
 //require and initialize mongodb
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_LOGIN, {
@@ -156,6 +156,9 @@ app.post("/", function(req, res) {
 
 
 //-------------------------------------------------------------------
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, function() {
+  console.log("Server started on port ${PORT}" );
 });
